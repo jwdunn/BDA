c263d350f16261c07cecbe42863544b230896e45
gasparyn <kenyangas@gmail.com>
2011-12-12 21:51:51 -0500

    Revert "checking"
    This reverts commit da0be4eabb1c2e87be229be6e5d2a9c068f648cb.

--- src/com/BeerDropper/www/DropOff.java
+++ src/com/BeerDropper/www/DropOff.java
@@ -4,6 +4,7 @@ package com.BeerDropper.www;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
